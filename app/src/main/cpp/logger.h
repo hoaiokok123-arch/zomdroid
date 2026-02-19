c4c360cc36d679a6fdfbbf1bcae4be3bfb16b47e
tuandz90212-cmd <tuandz90212@gmail.com>
2026-02-19 15:39:24 +0700

    Fix LOGW/LOGI format macro expansion for C compiler

--- app/src/main/cpp/logger.h
+++ app/src/main/cpp/logger.h
@@ -9,10 +9,10 @@
 
 #define LOGF(fmt, ...) _LOG(ANDROID_LOG_FATAL, "[%s] " fmt, __func__, ##__VA_ARGS__)
 #define LOGE(fmt, ...) _LOG(ANDROID_LOG_ERROR, "[%s] " fmt, __func__, ##__VA_ARGS__)
-#define LOGW(fmt, ...) _LOG(ANDROID_LOG_WARN, (fmt), ##__VA_ARGS__)
-#define LOGI(fmt, ...) _LOG(ANDROID_LOG_INFO, (fmt), ##__VA_ARGS__)
-#define LOGD(fmt, ...) _LOG(ANDROID_LOG_DEBUG, (fmt), ##__VA_ARGS__)
-#define LOGV(fmt, ...) _LOG(ANDROID_LOG_VERBOSE, (fmt), ##__VA_ARGS__)
+#define LOGW(fmt, ...) _LOG(ANDROID_LOG_WARN, fmt, ##__VA_ARGS__)
+#define LOGI(fmt, ...) _LOG(ANDROID_LOG_INFO, fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...) _LOG(ANDROID_LOG_DEBUG, fmt, ##__VA_ARGS__)
+#define LOGV(fmt, ...) _LOG(ANDROID_LOG_VERBOSE, fmt, ##__VA_ARGS__)
 #else
 #include <stdio.h>
 
@@ -21,10 +21,10 @@
 
 #define LOGF(fmt, ...) _LOG_STDERR("F", "[%s] " fmt, __func__, ##__VA_ARGS__)
 #define LOGE(fmt, ...) _LOG_STDERR("E", "[%s] " fmt, __func__, ##__VA_ARGS__)
-#define LOGW(fmt, ...) _LOG_STDERR("W", (fmt), ##__VA_ARGS__)
-#define LOGI(fmt, ...) _LOG_STDERR("I", (fmt), ##__VA_ARGS__)
-#define LOGD(fmt, ...) _LOG_STDERR("D", (fmt), ##__VA_ARGS__)
-#define LOGV(fmt, ...) _LOG_STDERR("V", (fmt), ##__VA_ARGS__)
+#define LOGW(fmt, ...) _LOG_STDERR("W", fmt, ##__VA_ARGS__)
+#define LOGI(fmt, ...) _LOG_STDERR("I", fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...) _LOG_STDERR("D", fmt, ##__VA_ARGS__)
+#define LOGV(fmt, ...) _LOG_STDERR("V", fmt, ##__VA_ARGS__)
 #endif
 
 #endif //ZOMDROID_LOGGER_H
