74be1322afc4545f0101d39e1c0cfedf9794ad44
tuandz90212-cmd <tuandz90212@gmail.com>
2026-02-19 15:37:17 +0700

    Fix portable variadic logging macros for iOS clang

--- app/src/main/cpp/logger.h
+++ app/src/main/cpp/logger.h
@@ -5,26 +5,26 @@
 #include <android/log.h>
 
 #define _LOG(priority, fmt, ...) \
-  ((void)__android_log_print((priority), (LOG_TAG), (fmt)__VA_OPT__(, ) __VA_ARGS__))
+  ((void)__android_log_print((priority), (LOG_TAG), (fmt), ##__VA_ARGS__))
 
-#define LOGF(fmt, ...) _LOG(ANDROID_LOG_FATAL, "[%s] " fmt, __func__ __VA_OPT__(, ) __VA_ARGS__)
-#define LOGE(fmt, ...) _LOG(ANDROID_LOG_ERROR, "[%s] " fmt, __func__ __VA_OPT__(, ) __VA_ARGS__)
-#define LOGW(fmt, ...) _LOG(ANDROID_LOG_WARN, (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGI(fmt, ...) _LOG(ANDROID_LOG_INFO, (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGD(fmt, ...) _LOG(ANDROID_LOG_DEBUG, (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGV(fmt, ...) _LOG(ANDROID_LOG_VERBOSE, (fmt)__VA_OPT__(, ) __VA_ARGS__)
+#define LOGF(fmt, ...) _LOG(ANDROID_LOG_FATAL, "[%s] " fmt, __func__, ##__VA_ARGS__)
+#define LOGE(fmt, ...) _LOG(ANDROID_LOG_ERROR, "[%s] " fmt, __func__, ##__VA_ARGS__)
+#define LOGW(fmt, ...) _LOG(ANDROID_LOG_WARN, (fmt), ##__VA_ARGS__)
+#define LOGI(fmt, ...) _LOG(ANDROID_LOG_INFO, (fmt), ##__VA_ARGS__)
+#define LOGD(fmt, ...) _LOG(ANDROID_LOG_DEBUG, (fmt), ##__VA_ARGS__)
+#define LOGV(fmt, ...) _LOG(ANDROID_LOG_VERBOSE, (fmt), ##__VA_ARGS__)
 #else
 #include <stdio.h>
 
 #define _LOG_STDERR(level, fmt, ...) \
-  ((void)fprintf(stderr, "[" level "][%s] " fmt "\n", LOG_TAG __VA_OPT__(, ) __VA_ARGS__))
+  ((void)fprintf(stderr, "[" level "][%s] " fmt "\n", LOG_TAG, ##__VA_ARGS__))
 
-#define LOGF(fmt, ...) _LOG_STDERR("F", "[%s] " fmt, __func__ __VA_OPT__(, ) __VA_ARGS__)
-#define LOGE(fmt, ...) _LOG_STDERR("E", "[%s] " fmt, __func__ __VA_OPT__(, ) __VA_ARGS__)
-#define LOGW(fmt, ...) _LOG_STDERR("W", (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGI(fmt, ...) _LOG_STDERR("I", (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGD(fmt, ...) _LOG_STDERR("D", (fmt)__VA_OPT__(, ) __VA_ARGS__)
-#define LOGV(fmt, ...) _LOG_STDERR("V", (fmt)__VA_OPT__(, ) __VA_ARGS__)
+#define LOGF(fmt, ...) _LOG_STDERR("F", "[%s] " fmt, __func__, ##__VA_ARGS__)
+#define LOGE(fmt, ...) _LOG_STDERR("E", "[%s] " fmt, __func__, ##__VA_ARGS__)
+#define LOGW(fmt, ...) _LOG_STDERR("W", (fmt), ##__VA_ARGS__)
+#define LOGI(fmt, ...) _LOG_STDERR("I", (fmt), ##__VA_ARGS__)
+#define LOGD(fmt, ...) _LOG_STDERR("D", (fmt), ##__VA_ARGS__)
+#define LOGV(fmt, ...) _LOG_STDERR("V", (fmt), ##__VA_ARGS__)
 #endif
 
 #endif //ZOMDROID_LOGGER_H
