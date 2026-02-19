8ba263fe52da346f4a57697127bccd02cf471a82
tuandz90212-cmd <tuandz90212@gmail.com>
2026-02-19 18:24:53 +0700

    ios2-jit: fix vfs shim and provide malloc header on darwin

---
+++ ios2/include/malloc.h
@@ -0,0 +1,7 @@
+#ifndef ZOMDROID_IOS_MALLOC_STUB_H
+#define ZOMDROID_IOS_MALLOC_STUB_H
+
+#include <stdlib.h>
+#include <malloc/malloc.h>
+
+#endif
--- ios2/include/sys/vfs.h
+++ ios2/include/sys/vfs.h
@@ -3,16 +3,11 @@
 
 #include <sys/mount.h>
 
-typedef struct statfs statfs64;
-
-static inline int statfs64(const char* path, struct statfs* buf)
-{
-    return statfs(path, buf);
-}
-
-static inline int fstatfs64(int fd, struct statfs* buf)
-{
-    return fstatfs(fd, buf);
-}
+#ifndef statfs64
+#define statfs64 statfs
+#endif
+#ifndef fstatfs64
+#define fstatfs64 fstatfs
+#endif
 
 #endif
